ea73d295d9126cd6d893bace2c2addf50ac1ba2a
Sergii Pechenizkyi <plastivreg@gmail.com>
2018-11-20 16:37:04 +0100

    Android 28

--- android-28/version-check.sh
+++ android-28/version-check.sh
@@ -16,8 +16,7 @@ emulator-check accel hyper-v cpu-info
 export SHELL=/bin/bash
 # If you want audio pass QEMU_AUDIO_DRV=pa -> https://www.wagner.pp.ru/fossil/vws/wiki?name=QEMU+audio
 export QEMU_AUDIO_DRV=none
-emulator -verbose -show-kernel -avd android-27-emulator -no-accel -no-audio -no-window -gpu swiftshader_indirect -skin 480x800 &
+emulator -verbose -show-kernel -avd android-28-emulator -no-accel -no-audio -no-window -gpu swiftshader_indirect -skin 480x800 &
 ./android-wait-for-emulator
-sleep 120
 adb devices
 adb shell pm dump com.google.android.gms | grep version
