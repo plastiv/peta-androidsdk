2e876795ba0eed618280abaaed94e7212a0ae528
Sergii Pechenizkyi <plastivreg@gmail.com>
2019-02-27 21:39:31 +0100

    Reformat start-emulator script

---
+++ android-emulator/start-emulator.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+IFS=$'\n\t'
+
+which sdkmanager
+which avdmanager
+which emulator
+which adb
+ldd `which emulator`
+java -Xmx32m -version
+sdkmanager --list
+emulator -list-avds
+emulator-check accel hyper-v cpu-info
+emulator -verbose -avd android-emulator-${EMULATOR_SDK_VERSION} \
+         -no-accel \
+         -no-audio \
+         -no-window \
+         -no-boot-anim \
+         -gpu swiftshader_indirect \
+         -memory 2048 \
+         -skin 480x800 \
+         & ./wait-for-boot-completed
+adb devices
+adb shell pm dump com.google.android.gms | grep version
