c8b4b0c9726fb1108e25fb397440f96e472a6805
Sergii Pechenizkyi <plastivreg@gmail.com>
2019-02-27 17:00:06 +0100

    Use env variable instead of hardcoded emulator sdk version

--- android-emulator/version-check.sh
+++ android-emulator/version-check.sh
@@ -15,7 +15,7 @@ emulator -list-avds
 emulator-check accel hyper-v cpu-info
 # If you want audio pass QEMU_AUDIO_DRV=pa -> https://www.wagner.pp.ru/fossil/vws/wiki?name=QEMU+audio
 export QEMU_AUDIO_DRV=none
-emulator -verbose -show-kernel -avd android-21-emulator -no-accel -no-audio -no-window -no-boot-anim -gpu swiftshader_indirect -memory 2048 -skin 480x800 &
+emulator -verbose -show-kernel -avd android-emulator-${EMULATOR_SDK_VERSION} -no-accel -no-audio -no-window -no-boot-anim -gpu swiftshader_indirect -memory 2048 -skin 480x800 &
 ./wait-for-boot-completed
 adb devices
 adb shell pm dump com.google.android.gms | grep version
