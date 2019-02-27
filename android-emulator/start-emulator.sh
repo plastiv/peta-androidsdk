0ae8f65d37c20599f07e7047187e8341d621fd42
Sergii Pechenizkyi <plastivreg@gmail.com>
2019-02-27 22:41:48 +0100

    Fix background piping and kill emulator at the and

--- android-emulator/start-emulator.sh
+++ android-emulator/start-emulator.sh
@@ -12,14 +12,17 @@ java -Xmx32m -version
 sdkmanager --list
 emulator -list-avds
 emulator-check accel hyper-v cpu-info
+# https://developer.android.com/studio/run/emulator-commandline#startup-options
 emulator -verbose -avd android-emulator-${EMULATOR_SDK_VERSION} \
          -no-accel \
          -no-audio \
          -no-window \
          -no-boot-anim \
+         -no-snapshot \
          -gpu swiftshader_indirect \
          -memory 2048 \
-         -skin 480x800 \
-         & ./wait-for-boot-completed
+         -skin 480x800 &
+./wait-for-boot-completed
 adb devices
 adb shell pm dump com.google.android.gms | grep version
+adb emu kill
