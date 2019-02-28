105e892463c7d96822bdb51065553b160521ab58
Sergii Pechenizkyi <plastivreg@gmail.com>
2019-02-28 10:09:31 +0100

    Disable camera

--- android-emulator/start-emulator.sh
+++ android-emulator/start-emulator.sh
@@ -16,10 +16,13 @@ emulator -verbose -avd android-emulator-${EMULATOR_SDK_VERSION} \
          -no-window \
          -no-boot-anim \
          -no-snapshot \
+         -camera-back none \
+         -camera-front none \
          -gpu swiftshader_indirect \
          -memory 2048 \
          -skin 480x800 &
 ./wait-for-boot-completed
 adb devices
 adb shell pm dump com.google.android.gms | grep version
+sleep 1 
 adb emu kill
