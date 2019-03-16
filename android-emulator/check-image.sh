9180af146290ffeeb5e9d6841f027ce239bb2974
Sergii Pechenizkyi <plastivreg@gmail.com>
2019-03-16 14:44:25 +0100

    Explicitly start adb service

--- android-emulator/check-image.sh
+++ android-emulator/check-image.sh
@@ -5,7 +5,9 @@ sdkmanager --list
 ./create-avd.sh
 avdmanager list avd
 emulator-check accel hyper-v cpu-info window-mgr desktop-env
+adb kill-server
 ./start-emulator.sh &
+adb start-server
 ./wait-for-boot-completed
 adb devices -l
 adb shell pm dump com.google.android.gms | grep version
